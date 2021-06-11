0edf544f123cfe60068f033814f5426ae8543af2
TemoSulava <sulava.temo@yahoo.com>
2021-06-11 17:12:57 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import text from "./data";
 
 function App() {
+  const [amount, setAmount] = useState("");
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const handleClick = () => {
+    if (amount > 9) {
+      setParagraphs(text.slice(0, text.length - 1));
+    } else if (amount < 1) {
+      setParagraphs(text.slice(-1));
+    } else {
+      setParagraphs(text.slice(0, amount));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAmount("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs :</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button className="btn" onClick={handleClick}>
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {paragraphs.map((paragraph, idx) => (
+          <p key={idx}>{paragraph}</p>
+        ))}
+      </article>
+    </section>
   );
 }
 
